2b20e10f6a517f18716a54de01ad23349f936f42
izzycyber331 <isaiah.smith2@my.utsa.edu>
2025-08-18 09:07:09 -0500

    Add files via upload

---
+++ ping_servers.py
@@ -0,0 +1,12 @@
+import os
+
+servers = ['google.com', 'github.com', '8.8.8.8']
+
+print("--- Pinging servers... ---")
+
+for server in servers:
+    response = os.system(f"ping -c 1 {server}")
+    if response == 0:
+        print(f"✅ {server} is up!")
+    else:
+        print(f"❌ {server} is down!")
